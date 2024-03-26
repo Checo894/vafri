b338fcc4f5b63723c0dbdb77450b56389d305d3a
Checo894 <A01068505@tec.mx>
2024-03-26 01:41:33 -0600

    pRODUCT lIST EN PROCESO

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import './ListProduct.css'
+import { useFetcher } from "react-router-dom";
+import { useEffect } from "react";
+import removeIcon from '../../assets/removeicon.svg'
 
 const ListProduct = () => {
+
+    const [allproducts,setAllProducts] = useState([]);
+
+    const fetchInfo = async ()=>{
+        await fetch('http://localhost:4000/allproducts')
+        .then((res)=>res.json())
+        .then((data)=>{setAllProducts(data)});
+    }
+
+    useEffect(()=>{
+        fetchInfo();
+    },[]);
+
     return(
         <div className="list-product">
-            
+            <h1>Lista de todos los productos</h1>
+            <div className="listproduct-format-main">
+                <p>Productos</p>
+                <p>FMSI</p>
+                <p>Marca</p>
+                <p>Fórmula</p>
+                <p>Medidas</p>
+                <p>Aplicaciones</p>
+                <p>Categoría</p>
+                {/* <p>PDF</p> */}
+                <p>Quitar un producto</p>
+            </div>
+            <div className="listproduct-allproducts">
+                <hr />
+                {allproducts.map((product,index)=>{
+                    return <> <div key={index} className="listproduct-format-main listproduct-format">
+                        <img src={product.image} alt="" className="listproduct-product-icon"/>
+                        <p>FMSI {product.fmsi}</p>
+                        <p>{product.brand}</p>
+                        <p>{product.formula}</p>
+                        <p>{product.measures}</p>
+                        <p>{product.application}</p>
+                        <p>{product.category}</p>
+                        {/* <p>{product.pdf}</p> */}
+                        {/* <img src={product.pdf} alt="" className="listproduct-product-pdf" style={{height: '80px'}}/> */}
+                        <img src={removeIcon} alt="" className="listproduct-remove-icon"/>
+                    </div>
+                    <hr />
+                    </>
+                })}
+            </div>
         </div>
     )
 }
