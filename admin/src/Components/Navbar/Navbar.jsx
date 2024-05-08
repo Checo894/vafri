9a3cf9117f7c5bcfc7c6447e922db874fbbfda51
unknown <sergut046@hotmail.com>
2024-05-08 03:35:48 -0600

    ruta default logo admin

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import {Link} from 'react-router-dom'
 import './Navbar.css'
 import navlogo from '../../assets/logo.png'
 
 const Navbar = () => {
     return (
         <div className="navbar">
-            <img src={navlogo} alt="" className="nav-logo" />
+            <Link to={'/'} style={{textDecoration: 'none'}}>
+                <img src={navlogo} alt="" className="nav-logo" />
+            </Link>
         </div>
     )
 }
