f78c52c5a50e0bbed1f832f0748c039b9c10d801
Sebastián Cervera Maltos <117860901+sebascm0906@users.noreply.github.com>
2024-07-08 10:36:00 -0600

    back a medias

--- backend/index.js
+++ backend/index.js
@@ -1,147 +1,122 @@
 require('dotenv').config();
 
 const port = 4000;
-const express = require("express")
+const express = require("express");
 const AWS = require("aws-sdk");
 const app = express();
-const mongoose = require("mongoose")
-const jwt = require("jsonwebtoken")
-const multer = require("multer")
-const path = require("path")
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const multerS3 = require('multer-s3');
+const path = require("path");
 const cors = require("cors");
-const { log } = require("console");
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 // Database connection with MongoDB 
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //API Creation 
-
-app.get("/", (req, res) =>{
-    res.send("Express App is Running")
-})
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb){
-        cb(null, 'upload/images')
-    },
-    filename:(req,file,cb) => {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
-
-
-const upload = multer({storage:storage})
+app.get("/", (req, res) => {
+    res.send("Express App is Running");
+});
 
 // Configura tus credenciales y región de AWS S3
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION
-  });
-  
-  const s3 = new AWS.S3();
+});
 
+const s3 = new AWS.S3();
 
-var multipleUploads = upload.fields([
-    { name: 'product', maxCount: 1 }, 
+// Configuración de multer para cargar directamente a S3
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'vafri',
+        acl: 'public-read',
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            const fileName = `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`;
+            cb(null, fileName);
+        }
+    })
+});
+
+const multipleUploads = upload.fields([
+    { name: 'product', maxCount: 1 },
     { name: 'productPDF', maxCount: 1 }
-])
+]);
 
-
-//Creating upload endpoint for images
-
-app.use('/images', express.static('upload/images'))
-app.post("/upload", upload.fields([{ name: 'product', maxCount: 1 }, { name: 'productPDF', maxCount: 1 }]), (req, res) => {
+// Creating upload endpoint for images
+app.post("/upload", multipleUploads, (req, res) => {
     const files = req.files;
     let responses = [];
 
-    const uploadFileToS3 = (file, cb) => {
-        const fileName = `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`;
-        const params = {
-            Bucket: 'vafri', // Asegúrate de reemplazar esto con el nombre de tu bucket
-            Key: fileName,
-            Body: file.buffer,
-            ACL: 'public-read'
-        };
-
-        s3.upload(params, function(err, data) {
-            if (err) {
-                console.log("Error uploading to S3: ", err);
-                return cb(err, null);
-            }
-            console.log("Upload successful: ", data);
-            cb(null, data.Location);
-        });
-    };
+    if (!files || Object.keys(files).length === 0) {
+        return res.status(400).send("No files were uploaded.");
+    }
 
     Object.keys(files).forEach(field => {
         files[field].forEach(file => {
-            uploadFileToS3(file, (err, location) => {
-                if (err) {
-                    res.status(500).send("Error uploading file.");
-                } else {
-                    responses.push({ field: field, url: location });
-                    if (responses.length === (files.product ? files.product.length : 0) + (files.productPDF ? files.productPDF.length : 0)) {
-                        res.json({ success: 1, files: responses });
-                    }
-                }
-            });
+            responses.push({ field: field, url: file.location });
         });
     });
+
+    res.json({ success: 1, files: responses });
 });
 
-//Schema for Creating Products
-
+// Schema for Creating Products
 const Product = mongoose.model("Product", {
-    id:{
-        type:Number,
+    id: {
+        type: Number,
         require: true,
     },
-    fmsi:{
-        type:String,
-        require:true,
-    },
-    brand:{
-        type:String,
-        require:true,
-    },
-    formula:{
-        type:String,
-        require:true,
-    },
-    measures:{
+    fmsi: {
         type: String,
-        require:false,
+        require: true,
     },
-    application:{
-        type:String,
-        require:false,
+    brand: {
+        type: String,
+        require: true,
     },
-    category:{
-        type:String,
-        require:true,
+    formula: {
+        type: String,
+        require: true,
     },
-    image:{
-        type:String,
-        require:true,
+    measures: {
+        type: String,
+        require: false,
     },
-    pdf:{
-        type:String,
-        require:false,
+    application: {
+        type: String,
+        require: false,
     },
-    date:{
+    category: {
+        type: String,
+        require: true,
+    },
+    image: {
+        type: String,
+        require: true,
+    },
+    pdf: {
+        type: String,
+        require: false,
+    },
+    date: {
         type: Date,
-        default:Date.now,
+        default: Date.now,
     },
-    available:{
-        type:Boolean,
+    available: {
+        type: Boolean,
         default: true,
     },
-})
+});
 
 app.post('/addproduct', async (req, res) => {
     const { id, fmsi, brand, category, application, formula, measures, image, pdf } = req.body;
@@ -165,20 +140,18 @@ app.post('/addproduct', async (req, res) => {
     }
 });
 
-//Creating API for dleeting products
-
+// Creating API for deleting products
 app.post('/removeproduct', async (req, res) => {
-    await Product.findOneAndDelete({id:req.body.id});
+    await Product.findOneAndDelete({ id: req.body.id });
     console.log("Removed");
     res.json({
-        success:true,
-        fmsi:req.body.fmsi,
-    })
-})
+        success: true,
+        fmsi: req.body.fmsi,
+    });
+});
 
-//Creating API for getting all products
-
-app.get('/allproducts', async (req, res)=>{
+// Creating API for getting all products
+app.get('/allproducts', async (req, res) => {
     const sortParam = req.query.sort;
     let sortOptions = {};
 
@@ -199,27 +172,25 @@ app.get('/allproducts', async (req, res)=>{
 
     const products = await Product.find({}).sort(sortOptions);
     res.send(products);
-})
+});
 
-//Creating Endpoint for Nuevo
-app.get('/nuevo', async (req, res)=>{
+// Creating Endpoint for Nuevo
+app.get('/nuevo', async (req, res) => {
     let products = await Product.find();
     let nuevo = products.slice(1).slice(-8);
     console.log("Nuevo Fetched");
     res.send(nuevo);
-})
+});
 
-//Creating Endpoint for Pasta products 
-app.get('/pastaP', async (req, res)=>{
-    let products = await Product.find({category:"pasta"});
-    let pastaP = products.slice(0,4);
+// Creating Endpoint for Pasta products 
+app.get('/pastaP', async (req, res) => {
+    let products = await Product.find({ category: "pasta" });
+    let pastaP = products.slice(0, 4);
     console.log("Pasta Fetched");
     res.send(pastaP);
-})
+});
 
-//Creating Endpoint for Related products
-
-
+// Creating Endpoint for Related products
 app.get('/related', async (req, res) => {
     const productId = req.query.productId;
     if (!productId) {
@@ -245,14 +216,16 @@ app.get('/related', async (req, res) => {
     }
 });
 
+// Middleware for handling errors
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
-
-
-app.listen(port, (error)=>{
-    if (!error){
-        console.log("Server Running on Port " +port)
+app.listen(port, (error) => {
+    if (!error) {
+        console.log("Server Running on Port " + port);
     } else {
-        console.log("Error: "+error)
+        console.log("Error: " + error);
     }
-})
-
+});
