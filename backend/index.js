c8a926a7a7d52b64a07d0963db27726889ef8c14
Sebastián Cervera Maltos <117860901+sebascm0906@users.noreply.github.com>
2024-07-08 11:04:56 -0600

    Esperemos ya S3 por fa sueltame

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ const s3 = new AWS.S3();
 const upload = multer({
     storage: multerS3({
         s3: s3,
-        bucket: 'vafri',
+        bucket: 'vafri-nuevo/images/images',
         acl: 'public-read',
         metadata: function (req, file, cb) {
             cb(null, { fieldName: file.fieldname });
