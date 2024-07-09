cb5de0abfb27f0ffdbe6068e93da2c9d543f49d7
Sebastián Cervera Maltos <117860901+sebascm0906@users.noreply.github.com>
2024-07-09 10:19:49 -0600

    patch

--- client/src/Components/Pasta/Pasta.jsx
+++ client/src/Components/Pasta/Pasta.jsx
@@ -1,42 +1,45 @@
-import React, {useEffect, useState} from 'react'
-import './Pasta.css'
-import Item from '../Item/Item'
+import React, { useEffect, useState } from 'react';
+import './Pasta.css';
+import Item from '../Item/Item';
+import all_data from '../Assets/vafri-products-new.js'; // Importa los datos locales
 
 const Pasta = () => {
 
     const [pastaP, setPastaP] = useState([]);
 
     useEffect(() => {
-        fetch('https://vafri-backend.vercel.app/pastaP')
-        .then(res => res.json())
-        .then(data => {
-            setPastaP(data);
-        })
-    },[]);
+        // Filtrar los productos que pertenecen a la categoría "pasta"
+        const pastaProducts = all_data.filter(item => item.category === 'pasta');
+        const lastFourPastaProducts = pastaProducts.slice(-4);
+        setPastaP(lastFourPastaProducts);
+    }, []);
 
     return(
         <div className="pasta">
             <h1>Pasta</h1>
             <hr />
             <div className="pasta-item">
-                {pastaP.map((item,i)=>{
+                {pastaP.map((item, i) => {
                     let applicationText = Array.isArray(item.application) && item.application.length === 2 ? item.application.join(', ') : item.application;
-                    return <Item 
-                                key={i} 
-                                id={item.id} 
-                                fmsi={item.fmsi}
-                                brand={item.brand}
-                                category={item.category} 
-                                model={item.model} 
-                                application={applicationText} 
-                                image={item.image} 
-                                measures={item.measures} 
-                                formula={item.formula}
-                                pdf={item.pdf} />
+                    return (
+                        <Item 
+                            key={i} 
+                            id={item.id} 
+                            fmsi={item.fmsi}
+                            brand={item.brand}
+                            category={item.category} 
+                            model={item.model} 
+                            application={applicationText} 
+                            image={item.image} 
+                            measures={item.measures} 
+                            formula={item.formula}
+                            pdf={item.pdf} 
+                        />
+                    );
                 })}
             </div>
         </div>
-    )
+    );
 }
 
-export default Pasta
+export default Pasta;
--- client/src/Components/PastaTacon/PastaTacon.jsx
+++ client/src/Components/PastaTacon/PastaTacon.jsx
@@ -1,19 +1,63 @@
+// import React, { useEffect, useState } from "react";
+// import './PastaTacon.css'
+// import Item from "../Item/Item";
+
+// const PastaTacon = () => {
+
+//     const [nuevo, setNuevo] = useState([]);
+
+//     useEffect(() => {   
+//         fetch('https://vafri-backend.vercel.app/nuevo')
+//         .then(res => res.json())
+//         .then(data => {
+//             setNuevo(data);
+//         })
+//     },[])
+
+
+//     return(
+//         <div className="pasta-tacon">
+//             <h1>Novedades en la tienda</h1>
+//             <hr />
+            
+//             <div className="nuevo-products">
+//             {nuevo.map((item,i)=>{
+//                     let appText = Array.isArray(item.application) && (item.application.length === 2 || item.application.length === 3)? item.application.join(', ') : item.application;
+//                     return <Item 
+//                                 key={i} 
+//                                 id={item.id} 
+//                                 fmsi={item.fmsi}
+//                                 brand={item.brand}
+//                                 category={item.category} 
+//                                 model={item.model}  
+//                                 image={item.image} 
+//                                 measures={item.measures} 
+//                                 application={appText}
+//                                 formula={item.formula}
+//                                 pdf={item.pdf} 
+//                                 />
+//                 })}
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default PastaTacon
+
 import React, { useEffect, useState } from "react";
-import './PastaTacon.css'
+import './PastaTacon.css';
 import Item from "../Item/Item";
+import all_data from '../Assets/vafri-products-new.js'; // Importa los datos locales
 
 const PastaTacon = () => {
 
     const [nuevo, setNuevo] = useState([]);
 
-    useEffect(() => {   
-        fetch('https://vafri-backend.vercel.app/nuevo')
-        .then(res => res.json())
-        .then(data => {
-            setNuevo(data);
-        })
-    },[])
-
+    useEffect(() => {
+        // Obtener los últimos 8 productos
+        const lastEightProducts = all_data.slice(-8);
+        setNuevo(lastEightProducts);
+    }, []);
 
     return(
         <div className="pasta-tacon">
@@ -21,25 +65,27 @@ const PastaTacon = () => {
             <hr />
             
             <div className="nuevo-products">
-            {nuevo.map((item,i)=>{
-                    let appText = Array.isArray(item.application) && (item.application.length === 2 || item.application.length === 3)? item.application.join(', ') : item.application;
-                    return <Item 
-                                key={i} 
-                                id={item.id} 
-                                fmsi={item.fmsi}
-                                brand={item.brand}
-                                category={item.category} 
-                                model={item.model}  
-                                image={item.image} 
-                                measures={item.measures} 
-                                application={appText}
-                                formula={item.formula}
-                                pdf={item.pdf} 
-                                />
+                {nuevo.map((item, i) => {
+                    let appText = Array.isArray(item.application) && (item.application.length === 2 || item.application.length === 3) ? item.application.join(', ') : item.application;
+                    return (
+                        <Item 
+                            key={i} 
+                            id={item.id} 
+                            fmsi={item.fmsi}
+                            brand={item.brand}
+                            category={item.category} 
+                            model={item.model}  
+                            image={item.image} 
+                            measures={item.measures} 
+                            application={appText}
+                            formula={item.formula}
+                            pdf={item.pdf} 
+                        />
+                    );
                 })}
             </div>
         </div>
     )
 }
 
-export default PastaTacon
+export default PastaTacon;
